a3ae53f689d36b9de57973079c61f3323707e6bb
danieldeab <22302072@live.uem.es>
2025-11-20 15:10:13 +0100

    Panel de administrador: se añaden botones NavigationBar

--- view/AdminPaneView.py
+++ view/AdminPaneView.py
@@ -1,4 +1,3 @@
-""""
 import flet as ft
 from view.BasePaneView import BaseUserView
 
@@ -6,12 +5,36 @@ class AdminPaneView(BaseUserView):
     def __init__(self, page: ft.Page):
         super().__init__(page)
 
+    def build_bottom_bar(self):
+        base_bar = super().build_bottom_bar()
 
-       #ejemplo
-        self.avisos = {
-            "titulo": "Panel de administración", "hora": "Acceso completo"
-        }
+        admin_destinations = [
+            ft.NavigationBarDestination(
+                icon=ft.Icons.MEMORY_OUTLINED,
+                selected_icon=ft.Icons.MEMORY,
+                label="Actuadores"
+            ),
+            ft.NavigationBarDestination(
+                icon = ft.Icons.STORAGE_OUTLINED,
+                selected_icon=ft.Icons.STORAGE,
+                label="Historial"
+            ),
+            ft.NavigationBarDestination(
+                icon=ft.Icons.PEOPLE_OUTLINED,
+                selected_icon=ft.Icons.PEOPLE,
+                label="Gestión de usuarios"
+            )
 
+        ]
 
-        En esta clase se añaden las diferencias del panel de administrador heredando de la estructura base
-"""
+        base_bar.destinations = admin_destinations + base_bar.destinations
+
+        return base_bar
+
+# main de prueba para previsualizar
+
+def main(page: ft.Page):
+    view = AdminPaneView(page)   # usar la clase hija
+    page.add(view.build())
+
+ft.app(target=main)
